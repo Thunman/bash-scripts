2b7f6c9332c658514fa8c86a3a259414a0151062
Daniel Thunman <Thunman42@gmail.com>
2024-05-12 13:52:14 +0200

    Update add-to-AD-fedora.sh

--- add-to-AD-fedora.sh
+++ add-to-AD-fedora.sh
@@ -8,6 +8,9 @@ read domain
 echo "Enter the IP of the Active Directory domain controller"
 read dc
 
+echo "Enter username for a user with admin rights in the domain"
+read admin
+
 #Get the interface name
 ### making a ugly fix to get it to run untill I figure out how to get the connection name
 #connection=$(nmcli -g GENERAL.CONNECTION device show $(nmcli device status | awk '/ethernet/ {print $1; exit}') | head -n1)
@@ -22,4 +25,4 @@ dnf install -y realmd sssd oddjob oddjob-mkhomedir adcli samba-common-tools
 
 #Join the domain
 realm discover $domain
-realm join $domain 
+realm join $domain -u $admin
